288947ba49a6adb6e342ef082866a9418db598f8
Om <135633997+hardbuilder@users.noreply.github.com>
2024-10-01 12:29:18 +0530

    Create Games.js

---
+++ Games.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Games() {
+  return (
+    <div id="games">
+      <h2>Games</h2>
+      <ul>
+        <li>Puzzle Game</li>
+        <li>Brain Teaser</li>
+        <li>Calming Game</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Games;
